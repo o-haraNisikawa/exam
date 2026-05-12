7b32a6ffcd4013fa324b96b87c946838df4a46d2
admin <mro2523020@stu.o-hara.ac.jp>
2026-05-12 10:51:34 +0900

    ５/12nisikawa

--- src/main/java/scoremanager/main/SubjectUpdateExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectUpdateExecuteAction.java
@@ -48,13 +48,8 @@ public class SubjectUpdateExecuteAction extends Action {
 
         // DAOで保存（更新 or 追加）
         SubjectDao sDao = new SubjectDao();
-
-        sDao.save(subject);
-        sDao.delete(subject);
-
         sDao.update(subject);
 
-        
         // 完了画面へ
         req.getRequestDispatcher("subject_update_done.jsp").forward(req, res);
     }
